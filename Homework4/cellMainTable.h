dcd9a342ca3e1d99639f4d72f8a1e0213a4f87d2
Anronio Lizarraga <anroniolizarraga@Anronios-Mac.local>
2017-09-26 22:14:15 -0600

    Cell objects were connected

---
+++ Homework4/cellMainTable.h
@@ -0,0 +1,16 @@
+//
+//  cellMainTable.h
+//  Homework4
+//
+//  Created by Anronio Lizarraga on 9/26/17.
+//  Copyright © 2017 Antonio Lizarraga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface cellMainTable : UITableViewCell
+@property (strong, nonatomic) IBOutlet UIImageView *imgUser;
+@property (strong, nonatomic) IBOutlet UILabel *lblName;
+@property (strong, nonatomic) IBOutlet UILabel *lblAge;
+
+@end
---
+++ Homework4/cellMainTable.m
@@ -0,0 +1,24 @@
+//
+//  cellMainTable.m
+//  Homework4
+//
+//  Created by Anronio Lizarraga on 9/26/17.
+//  Copyright © 2017 Antonio Lizarraga. All rights reserved.
+//
+
+#import "cellMainTable.h"
+
+@implementation cellMainTable
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
