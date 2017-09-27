68a4d0bf0adbef541264e4583be483d7b5649458
Anronio Lizarraga <anroniolizarraga@Anronios-Mac.local>
2017-09-27 00:20:30 -0600

    Segue done

---
+++ Homework4/AddCharacter.h
@@ -0,0 +1,15 @@
+//
+//  AddCharacter.h
+//  Homework4
+//
+//  Created by Anronio Lizarraga on 9/26/17.
+//  Copyright © 2017 Antonio Lizarraga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddCharacter : UIViewController 
+
+
+
+@end
---
+++ Homework4/AddCharacter.m
@@ -0,0 +1,41 @@
+//
+//  AddCharacter.m
+//  Homework4
+//
+//  Created by Anronio Lizarraga on 9/26/17.
+//  Copyright © 2017 Antonio Lizarraga. All rights reserved.
+//
+
+#import "AddCharacter.h"
+
+@interface AddCharacter ()
+
+@end
+
+@implementation AddCharacter
+
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+
+@end
